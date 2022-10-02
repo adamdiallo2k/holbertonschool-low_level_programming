e17d9c5c846604e6a5d9b0ca6fa927b9149b4285
Adam <adamdiallo2k@gmail.com>
2022-10-02 13:05:22 -0700

    hello,puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char greetings[] = "Programming is like building a multilingual puzzle";
+        puts(greetings);
+        return (0);
+}
