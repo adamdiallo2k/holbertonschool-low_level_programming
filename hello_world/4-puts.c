7018c2182dbe02deacd0140ecd38d0f36c38ecb9
Adam <adamdiallo2k@gmail.com>
2022-10-02 13:23:11 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-char greetings[] = "Programming is like building a multilingual puzzle";	 	puts(greetings);
+char greetings[] = "Programming is like building a multilingual puzzle";
+	puts(greetings);
 return (0);
 }
