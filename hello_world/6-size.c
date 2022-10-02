ecb1a7042c11a30acac7f6413ee774d63ee7a89f
Adam <adamdiallo2k@gmail.com>
2022-10-02 14:58:19 -0700

    test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int x;
 	char y;
+
 	y = 'a';
 	x = 10;
 	long unlong = 123;
 	float unfloat = 12.5;
 	long long int deuxlong = 124;
-	char textB[] = "byte(s)"
-	printf("size of a char : %d\n", sizeof(y),textB;
-	printf("size of a int : %i\n", sizeof(x), "byte(s)");
-	printf("size of a long : %ld\n", sizeof(unlong), "byte(s)");
-	printf("size of a long long int : %lli\n", sizeof(deuxlong), "byte(s)");
-	printf("size of a float : %d\n", sizeof(unfloat), "byte(s)");	
+
+	printf("size of a char : %c byte(s)\n", sizeof(y)),
+	printf("size of a int : %i byte(s)\n", sizeof(x)),
+	printf("size of a long : %Lf byte(s)\n", sizeof(unlong)),
+	printf("size of a long long int : %lli byte(s)\n", sizeof(deuxlong)),
+	printf("size of a float : %e byte(s)\n", sizeof(unfloat));	
 	return (0);
 }
