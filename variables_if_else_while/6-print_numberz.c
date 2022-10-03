6a7e5d6770b56b8291057649a9ea68db8dad6adf
Adam <adamdiallo2k@gmail.com>
2022-10-03 08:13:44 -0700

    test

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int x;
+
+for (x = 48; x <= 57; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+
+return (0);
+}
+
