46133efdf59ec97bd0d58d132dc97dfa9c5b555f
Adam <adamdiallo2k@gmail.com>
2022-10-03 06:21:04 -0700

    alphabet exept q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char x;
+char y;
+for (x = 'a'; x <= 'z'; x++)
+{
+	if (x == 'q' | x == 'e')
+	{
+	y++;
+	}
+	else
+	{
+	putchar(x);
+	}
+}
+	putchar('\n');
+
+	return (0);
+}
