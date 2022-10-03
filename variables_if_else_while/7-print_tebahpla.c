c669809a2d6bc0713e8a23863f25da28995ba167
Adam <adamdiallo2k@gmail.com>
2022-10-03 09:35:00 -0700

    test

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<ctype.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (int x = 122; x >= 97; x--)
+{
+	putchar(tolower(x));
+}
+	putchar('\n');
+	return (0);
+}
