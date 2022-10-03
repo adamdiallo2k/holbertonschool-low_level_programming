1bff1c433ea2edcf22d0713a05876f0f3bb8188d
Adam <adamdiallo2k@gmail.com>
2022-10-03 06:57:37 -0700

    test

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char x;
+
+for (x = '0'; x < ':'; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+
+return (0);
+}
+
