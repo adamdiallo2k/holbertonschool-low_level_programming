0c6a7dd06b5f2e34ce523ede61a4e763dfbccc36
Adam <adamdiallo2k@gmail.com>
2022-10-03 05:50:11 -0700

    alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+for (int x = 'a'; x <= 'z'; x++)
+{
+
+	putchar(x);
+
+	if (x == 'z')
+	{
+		for (int y = 'a'; y <= 'z'; y++)
+	{
+		putchar(toupper(y));
+	}
+
+}
+}
+
+	putchar('\n');
+
+	return (0);
+}
