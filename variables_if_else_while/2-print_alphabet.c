75d8d98af7d4bb2d8b48bd300c8afa75506a107f
Adam <adamdiallo2k@gmail.com>
2022-10-03 04:40:29 -0700

    A-Z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+for (int x = 'a'; x <= 'z'; x++)
+{
+	putchar(x);
+}
+	return (0);
+}
