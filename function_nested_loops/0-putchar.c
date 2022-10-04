97be5520a269416777732eeda471c6290afbd276
Adam <adamdiallo2k@gmail.com>
2022-10-04 06:55:29 -0700

    use new function putchar

---
+++ function_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char output[]  = "_putchar";
+int i;
+int arrayLength = 8;
+for (i = 0 ; i < arrayLength ; i++)
+{
+	_putchar(output[i]);
+}
+	_putchar('\n');
+
+return (0);
+}
