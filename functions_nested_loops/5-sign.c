c1e8c4534d00764046f17fb78bd38284ee9fc804
Adam <adamdiallo2k@gmail.com>
2022-10-05 16:06:35 -0700

    +0+-

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_sign - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @c: int parameter
+*/
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar(0);
+	return (0);
+}
+else if(n < 0)
+{
+	_putchar('-');
+	return(-1);
+	
+}
+
+
+}
