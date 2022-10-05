e872573534d6dc88acf968b62e14cd5838c6371a
Adam <adamdiallo2k@gmail.com>
2022-10-05 15:20:08 -0700

    add file to lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+
+
+if ( c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+else 
+{
+	return (0);
+}
+
+
+}
