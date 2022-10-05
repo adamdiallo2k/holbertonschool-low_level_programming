61c9bba4ee54fc6ce6cea9b2055274fa85c447a6
Adam <adamdiallo2k@gmail.com>
2022-10-05 14:52:59 -0700

    add description

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
 void print_alphabet(void)
 {
 char c;
 
-	for (c = 'a';c <= 'z';c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
-				
+
 	}
 	_putchar('\n');
-	
+
 
 }
-
-
