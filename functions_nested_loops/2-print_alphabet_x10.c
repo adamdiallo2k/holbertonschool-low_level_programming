21451f7b51262aa8f628b012fc09b43a474ab1ee
Adam <adamdiallo2k@gmail.com>
2022-10-05 15:03:12 -0700

    push alphabet x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+char i;
+char c;
+	for (i = 0; i <= 9; i++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+	_putchar('\n');
+	}
+
+}
