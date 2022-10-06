c991f016b389bae8efe645d07ddb6e0673344a40
Adam <adamdiallo2k@gmail.com>
2022-10-06 04:52:14 -0700

    test

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_last_digit - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @n: int parameter
+*/
+int _abs(int c)
+{
+
+
+if (c < 0)
+{
+        c = c * -1;
+        return (c);
+}
+else
+{
+        return (c);
+}
+return (0);
+
+}
+
+
+int print_last_digit(int n)
+{
+
+        int lastdigit = _abs(n) % 10;
+        return(lastdigit);
+}
+
