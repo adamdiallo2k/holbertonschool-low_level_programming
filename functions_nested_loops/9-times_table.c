2ff3087f4c418b228a4cbd2fdbf3ef65d127ca3b
Adam <adamdiallo2k@gmail.com>
2022-10-07 14:34:01 -0700

    test

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* times_table - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+int tablen;
+int n;
+int result;
+for (tablen = 0 ; tablen <= 10 ; tablen++)
+{
+	for (n = 0; n < 10; n++)
+	{
+	result = tablen * n;
+	printf(" %d ", result);
+	if (n != 9)
+	{
+	printf(",");
+	printf(" ");
+	printf(" ");
+	}
+	}
+	printf("\n");
+}
+}
