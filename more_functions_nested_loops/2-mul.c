8c900a29d9c3d453075ddec5b08f5a194ebe457b
Adam <adamdiallo2k@gmail.com>
2022-10-09 04:21:27 -0700

    multiplies

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* mul - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @a: int parameter
+* @b: int parameter
+*/
+
+int mul(int a, int b)
+{
+
+int result;
+result = a * b;
+return (result);
+
+
+}
