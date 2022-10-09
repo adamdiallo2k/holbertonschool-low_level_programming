70b529c4575b45230e5541eb32fcd0ad61e90760
Adam <adamdiallo2k@gmail.com>
2022-10-09 13:38:00 -0700

    more number x 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* print_most_numbers - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+void more_numbers(void)
+{
+int i;
+int firstDigit;
+int y;
+int lastdigit;
+int x;
+for (i = 0; i <= 14; i++)
+{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	lastdigit = y;
+	x = y;
+	while (x >= 10)
+	{
+	x = x / 10;
+	}
+	firstDigit = x;
+	_putchar(firstDigit + '0');
+	_putchar((lastdigit % 10) + '0');
+	}
+	else if (y <= 9)
+	{
+	_putchar(y + '0');
+	}
+
+	}
+	_putchar('\n');
+}
+}
