953eead73588975a38870f6b986fb76409cdbb09
Adam <adamdiallo2k@gmail.com>
2022-10-09 05:37:34 -0700

    test

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_diagonal - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @n: int parameter
+*/
+
+void print_diagonal(int n)
+{
+int i;
+int c;
+for (i = 0; i < n; i++)
+{
+	for (c = 0; c < i; c++)
+	{
+	_putchar(' ');
+	}
+	_putchar(47);
+
+	_putchar('\n');
+}
+
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_line(int n);
 int _putchar(char c);
+void print_diagonal(int n);
