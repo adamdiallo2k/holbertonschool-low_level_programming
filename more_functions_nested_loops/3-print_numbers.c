30e811fd2356988e3950cf37c74caa55830b6c78
Adam <adamdiallo2k@gmail.com>
2022-10-09 12:54:50 -0700

    diplay numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+}
+
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,3 +7,5 @@ int mul(int a, int b);
 void print_line(int n);
 int _putchar(char c);
 void print_diagonal(int n);
+void print_triangle(int size);
+void print_numbers(void);
