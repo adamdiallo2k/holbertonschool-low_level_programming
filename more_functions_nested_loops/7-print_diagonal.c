16f85519fc15ed96c272ca7c456942d490353de1
Adam <adamdiallo2k@gmail.com>
2022-10-09 05:51:20 -0700

    test

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,6 +11,11 @@ void print_diagonal(int n)
 {
 int i;
 int c;
+if( n <= 0)
+{
+_putchar('\n');
+}
+else {
 for (i = 0; i < n; i++)
 {
 	for (c = 0; c < i; c++)
@@ -21,6 +26,6 @@ for (i = 0; i < n; i++)
 
 	_putchar('\n');
 }
-
-	
 }
+}	
+
