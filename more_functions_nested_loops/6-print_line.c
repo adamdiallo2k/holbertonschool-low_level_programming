15222b7f88735191aac22f7b6f8f7b72c5feaffb
Adam <adamdiallo2k@gmail.com>
2022-10-09 05:09:58 -0700

    test

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @n: int parameter
+*/
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+	_putchar('_');
+}
+	_putchar('$');
+	_putchar('\n');
+}
+
