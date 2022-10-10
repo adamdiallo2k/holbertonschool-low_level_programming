49b588e6f17ff39ead3e64d513dcb7d50be728a0
Adam <adamdiallo2k@gmail.com>
2022-10-10 11:17:17 -0700

    test

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _puts - Entrypoint
+* Descritption: 'program to print a string''
+* Return: Always 0.
+* @str: char parameter
+*/
+void _puts(char *str)
+{
+	int count = 0;
+	while (str[count] != '\0')
+	{
+	_putchar(str[count]);
+	count++;
+	}
+	
+	_putchar('\n');
+}
+
+
