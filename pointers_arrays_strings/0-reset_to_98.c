7268fb4ab8dd06d6d691ef25b0e912ad6e98cd4c
Adam <adamdiallo2k@gmail.com>
2022-10-10 05:24:13 -0700

    test

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* reset_to_98 - check the code
+* Descritption: 'program to change value of pointer'
+* Return: Always 0.
+* @n: int parameter
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
-	
 }
