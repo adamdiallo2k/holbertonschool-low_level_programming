add31081c93f200f5e537db005ee4c97309c7acc
Adam <adamdiallo2k@gmail.com>
2022-10-10 06:56:28 -0700

    count lenght

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* strlen - Entrypoint
+* Descritption: 'program to '
+* Return: Always 0.
+* @s: char parameter
+*/
+ int _strlen(char *s)
+{
+	
+	
+	int count = 0;
+	while (s[count] != '\0')
+{
+  count++;
+}
+	return count;
+
+}
+
