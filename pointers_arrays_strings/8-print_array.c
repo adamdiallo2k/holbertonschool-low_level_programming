392b77d45731ca483c4c17704745afcba715e566
Adam <adamdiallo2k@gmail.com>
2022-10-11 07:05:08 -0700

    test

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - Entrypoint
+* Descritption: 'program to print a string''
+* Return: Always 0.
+* @a: int parameter
+* @n: int parameter
+*/
+void print_array(int *a, int n)
+{
+
+	int count = 0;
+	int i;
+	int lenght;
+	while (a[count] != '\0')
+	{
+	count++;
+	}
+	
+	for (i = 0 ; i <= (n - 1); i++) 
+	{
+	printf("%d, ",a[i]);
+	}
+	
+}
+
