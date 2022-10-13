50f609116574b8da60d294b678c23a14ebeaa7d6
Adam <adamdiallo2k@gmail.com>
2022-10-13 01:17:23 -0700

    fix 9 task

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,14 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
-	int i = 0;
+	
+	int i;
 
-	while (src[count] != '\0')
-	{
-	count++;
-	}
 
-	for (i = 0; i < count && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	dest[i] = src[i];
-	for ( ; i < count; i++)
+
+
 	dest[i] = '\0';
 
 	return (dest);
