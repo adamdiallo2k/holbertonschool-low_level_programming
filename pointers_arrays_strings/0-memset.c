e5cec758d474cbc68903824bc9bfbf7cd9b196e4
Adam <adamdiallo2k@gmail.com>
2022-10-14 15:01:02 -0700

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _memset - Entrypoint
+* Descritption: 'program to print a string''
+* Return: Always 0.
+* @s: char parameter
+* @b : char parameter
+* @n : int paramameter
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		s[i] = b;
+		i++;
+	}
+	printf("\n");
+
+	return (s);
+}
+
