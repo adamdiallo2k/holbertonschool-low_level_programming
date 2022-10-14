21342f65fcdca83dd6a9b9b1067eef7a8ca6a93e
Adam <adamdiallo2k@gmail.com>
2022-10-14 00:39:07 -0700

    erase space

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - print an array of integers
+ * @c: an array of char
+ * Return: nothing.
+ */
+char *cap_string(char *c)
+{
+int count = 0;
+
+while (c[count] != '\0')
+{
+
+if (c[count] == '\t')
+{
+c[count] = ' ';
+}
+
+	if ((c[count - 1] == ' ' || c[count - 1] == '\n') &&
+	(c[count] >= 'a' && c[count] <= 'z'))
+
+	{
+		c[count] = c[count] - 32;
+	}
+
+
+	count++;
+	}
+return (c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include <unistd.h>
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
