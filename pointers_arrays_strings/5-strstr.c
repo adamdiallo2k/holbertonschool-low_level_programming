b671ed71abdce3e04e4195c6e8b48bea1e01ccd2
Adam <adamdiallo2k@gmail.com>
2022-10-16 15:09:10 -0700

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strstr - Entrypoint
+* Descritption: 'program to print a string'
+* Return: Always 0.
+* @haystack: char parameter
+* @needle: char parameter
+*/
+
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	unsigned int y = 0;
+	unsigned int reponse = 1;
+	char *b = NULL;
+
+	while (i == 0)
+	{
+		reponse = 0;
+
+		for (y = 0; needle[y] != '\0'; y++)
+		{
+			if (needle[y] == haystack[i])
+			{
+			b = &haystack[0];
+			return (b);
+			}
+
+		}
+		return (needle);
+
+		i++;
+	}
+
+
+	return (needle);
+}
+
