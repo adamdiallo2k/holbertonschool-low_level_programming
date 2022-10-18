4535210adee1f6ac56d9d1b3c5ce35b82a4c4e66
Adam <adamdiallo2k@gmail.com>
2022-10-18 09:21:05 -0700

    test

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_pow_recursion - Entrypoint
+* Descritption: 'program to print a string''
+* Return: Always 0.
+* @x: int parameter
+* @i: int parameter
+*/
+
+int _pow_recursion(int x, int i)
+{
+	if (x == (i * i))
+	return (i);
+
+	else if (x > (i * i))
+	return (_pow_recursion(x, i + 1));
+
+	else if (x < (i * i))
+	return (-1);
+}
+
+/**
+*_sqrt_recursion - Entrypoint
+* Descritption: 'program to print a string''
+* Return: Always 0.
+* @n: int parameter
+*/
+int _sqrt_recursion(int n)
+{
+
+
+	return (_pow_recursion(n, 0));
+}
