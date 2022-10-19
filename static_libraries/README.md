3c23e27124824d4f21d8c1d525ca141230d7ca37
Adam <adamdiallo2k@gmail.com>
2022-10-19 05:44:29 -0700

    test

--- static_libraries/README.md
+++
@@ -1,1 +0,0 @@
-this folder is about the Recursion
--- static_libraries/main.h
+++ static_libraries/main.h
@@ -9,3 +9,8 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _atoi(char *s);
+int _isdigit(int c);
+int _abs(int n);
+int _islower(int c);
+int _isupper(int c);
