5927a5a0b41ee597c7fb93c98a4a4bd46c8a6e05
Adam <adamdiallo2k@gmail.com>
2022-10-19 06:59:12 -0700

    fix betty

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,4 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <unistd.h>
+
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
@@ -14,3 +17,4 @@ int _isdigit(int c);
 int _abs(int n);
 int _islower(int c);
 int _isupper(int c);
+#endif
