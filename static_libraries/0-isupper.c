6024157373c359fb1a7c0e598487b6c5a13ebdb0
Adam <adamdiallo2k@gmail.com>
2022-10-19 06:15:17 -0700

    test

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _isupper - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @c: int parameter
+*/
+
+int _isupper(int c)
+{
+
+
+if  (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+
+
+}
---
+++ static_libraries/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _abs - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @c: int parameter
+*/
+
+int _abs(int c)
+{
+
+
+if (c < 0)
+{
+	c = c * -1;
+	return (c);
+}
+else
+{
+	return (c);
+}
+return (0);
+
+}
