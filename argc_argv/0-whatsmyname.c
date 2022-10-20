9b76f6a54a8055e28bcc9f2ab53adf1be6b382c5
Adam <adamdiallo2k@gmail.com>
2022-10-20 04:25:39 -0700

    add folder

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @argc: int parameter
+* @argv: char parameter
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
