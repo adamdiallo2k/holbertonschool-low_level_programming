a5e53e7d810de8a9c7502dc7beb9a523a4c5ed8c
Adam <adamdiallo2k@gmail.com>
2022-10-21 01:21:24 -0700

    multiply arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @argc: int parameter
+* @argv: char parameter
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 3)
+	{
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	}
+
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	return (0);
+
+}
