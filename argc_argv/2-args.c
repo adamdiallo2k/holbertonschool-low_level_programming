6e9c6f1d4b3d9577b41cd545befa05414a1adcf9
Adam <adamdiallo2k@gmail.com>
2022-10-21 00:58:01 -0700

    print all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry pointddscxds
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+* @argc: int parameter
+* @argv: char parameter
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
