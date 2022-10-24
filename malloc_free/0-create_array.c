839f84d38688fbaef931438c799f9e01057b472f
Adam <adamdiallo2k@gmail.com>
2022-10-24 01:44:13 -0700

    add repo maloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* create_array - Entry point
+* Description: 'creates an array of chars'
+* Return: str
+* @size: unsigned int parameter
+* @c: char parameter
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i;
+
+	str = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	return (str);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+this folder is about malloc, free : to allocate and free memorie
