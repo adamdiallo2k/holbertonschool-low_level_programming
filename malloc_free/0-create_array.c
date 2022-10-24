b0d8caa113b2fe0b9f891245eefefb6fdad92aa9
Adam <adamdiallo2k@gmail.com>
2022-10-24 01:59:59 -0700

    add null condition

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,10 +12,15 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
+
 	if (size > 0)
 	{
+
 	str = malloc(sizeof(char) * size);
-	
+
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	}
@@ -23,7 +28,7 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 	str = NULL;
-	
+
 	}
 	return (str);
 }
