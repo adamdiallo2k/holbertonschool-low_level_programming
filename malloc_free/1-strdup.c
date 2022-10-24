1f598b49c7bbf34c9f3ce328f776f157e07512a6
Adam <adamdiallo2k@gmail.com>
2022-10-24 02:37:03 -0700

    add strrdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* _strlen - Entry point
+* Description: 'Return the lenght of a pointer'
+* Return: count
+* @s: char parameter
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+{
+	count++;
+}
+	return (count);
+}
+
+/**
+* _strdup - Entry point
+* Description: 'Write a function that returns a pointer'
+* Return: st2
+* @str: char parameter
+*/
+char *_strdup(char *str)
+{
+	int i;
+	char *st2;
+	int size = _strlen(str);
+
+	if (size > 0)
+	{
+		st2 = malloc(sizeof(char) * size);
+
+	if (st2 == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		st2[i] = str[i];
+	}
+	return (st2);
+	}
+}
