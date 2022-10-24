285e2475438503e0706e1e0f000a43d74b7dae2e
Adam <adamdiallo2k@gmail.com>
2022-10-24 08:57:57 -0700

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,10 +20,10 @@ char *str_concat(char *s1, char *s2)
 	int count2 = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	while (s2[count] != '\0')
 	{
 		count++;
