dcd3e17abec675bc0be2e41f1533a9fcb51cde69
Adam <adamdiallo2k@gmail.com>
2022-10-26 01:09:24 -0700

    add folder more malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* malloc_checked - Entry point
+* Description: 'Write a function that returns a pointer'
+* Return: s3
+* @b: int parameter
+*/
+
+void *malloc_checked(unsigned int b)
+{
+
+malloc(b);
+
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this folder is about to practice more of malloc and free functions
