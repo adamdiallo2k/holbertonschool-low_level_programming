b5ce330017288919cb1a3b0cb15056ec770fe5b8
Adam <adamdiallo2k@gmail.com>
2022-10-31 06:39:08 -0700

    copy struct value

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -26,11 +26,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(sizeof(char) * len1);
 	if (d->name == NULL)
 		return (NULL);
+	free(d->name);
 	d->owner = malloc(sizeof(char) * len2);
 	if (d->owner == NULL)
 		return (NULL);
+	free(d->owner);
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
+
 	return (d);
 }
