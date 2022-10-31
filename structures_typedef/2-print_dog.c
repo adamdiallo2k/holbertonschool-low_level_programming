11ad0a5abf19f562f4083d3cc3dcd0aa50d3ab24
Adam <adamdiallo2k@gmail.com>
2022-10-31 03:53:24 -0700

    test

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,6 +13,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 		printf("Name: %s", d->name);
 		printf("\n");
 		printf("Age: %f", d->age);
