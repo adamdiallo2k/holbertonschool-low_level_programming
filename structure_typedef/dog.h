848bbe7562cd02ec3a44ee02609b447ef7292a24
Adam <adamdiallo2k@gmail.com>
2022-10-31 01:58:15 -0700

    add new repo with the README

---
+++ structure_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a simple struct named dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
