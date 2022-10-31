673332fece4e8c0cd2e4ef4fa43a51c2e110c34e
Adam <adamdiallo2k@gmail.com>
2022-10-31 02:52:22 -0700

    test

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
@@ -15,8 +14,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	(*d).name = name;
 	d->age = age;
 	d->owner = owner;
+	}
+
 }
 
