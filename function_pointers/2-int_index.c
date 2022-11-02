b840020f5e7c904a8ad6ad47261eb666bde4698f
Adam <adamdiallo2k@gmail.com>
2022-11-02 07:47:19 -0700

    task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* int_index - Entry pointddscxds
+* Description: 'print a name'
+* Return: nothing
+* @array: int pointer parameter
+* @size: size_t parameter
+* @cmp:int function pointer parameter
+*/
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	if (array != NULL && size >= 0)
+	{
+
+		int i;
+		int (*d)(int);
+			d = cmp;
+
+		for (i = 0; i < size; i++)
+		{
+			if (d(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
