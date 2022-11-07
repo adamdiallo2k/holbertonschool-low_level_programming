eefc477ab0b57d213b45a2393f59951ecabb74bc
Adam <adamdiallo2k@gmail.com>
2022-11-07 09:14:31 -0800

    test

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -22,57 +22,54 @@ int op_add(int a, int b)
 * @a: int parameter
 * @b: int parameter
 */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-  int op_sub(int a, int b)
-  {
-          return (a - b);
-  }
+/**
+* op_mul - Entry point
+* Description: 'return product'
+* Return: nothing
+* @a: int parameter
+* @b: int parameter
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
- /**
-  * op_mul - Entry point
-  * Description: 'return product'
-  * Return: nothing
-  * @a: int parameter
-  * @b: int parameter
-  */
-
-    int op_mul(int a, int b)
-    {
-            return (a * b);
-    }
-
- /**
-  * op_div - Entry point
-  * Description: 'return division'
-  * Return: nothing
-  * @a: int parameter
-  * @b: int parameter
-  */
-  
-    int op_div(int a, int b)
-    {
+/**
+* op_div - Entry point
+* Description: 'return division'
+* Return: nothing
+* @a: int parameter
+* @b: int parameter
+*/
+int op_div(int a, int b)
+{
 	if (b == 0)
 	{
-    		printf("Error\n");
+		printf("Error\n");
 		exit(100);
-	}	
-            return (a / b);
-    }
+	}
+	return (a / b);
+}
 
- /**
-  * op_mod - Entry point
-  * Description: 'return difference'
-  * Return: nothing
-  * @a: int parameter
-  * @b: int parameter
-  */
+/**
+* op_mod - Entry point
+* Description: 'return difference'
+* Return: nothing
+* @a: int parameter
+* @b: int parameter
+*/
 
-    int op_mod(int a, int b)
-    {
-	    if (b == 0)
-	    {
-		    printf("Error\n");
-		    exit(100);
-	    }
-            return (a % b);
-    }
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
