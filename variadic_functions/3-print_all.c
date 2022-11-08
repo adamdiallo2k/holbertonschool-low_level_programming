3211f1858719d9e9a45348499e5437aaaf7e838c
Adam <adamdiallo2k@gmail.com>
2022-11-08 04:27:05 -0800

    testf

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,7 +10,51 @@
 * @format: char const parameter
 */
 
+void print_chare(va_list c)
+{
+	char *r = va_arg(c, char *);
 
+	printf("%s", r);
+}
+
+/**
+* print_float - Entry point
+* Description: 'return difference'
+* Return: nothing
+* @f: va_list parameter
+*/
+void print_float(va_list f)
+{
+	float p = va_arg(f, double);
+
+	printf("%f", p);
+}
+
+/**
+* print_char - Entry point
+* Description: 'return product'
+* Return: nothing
+* @c: va_list parameter
+*/
+void print_char(va_list c)
+{
+	int p = va_arg(c, int);
+
+	printf("%c", p);
+}
+
+/**
+* print_int - Entry point
+* Description: 'return division'
+* Return: nothing
+* @i: int parameter
+*/
+void print_int(va_list i)
+{
+	int p = va_arg(i, int);
+
+	printf("%i", p);
+}
 void print_all(const char * const format, ...)
 {
 
