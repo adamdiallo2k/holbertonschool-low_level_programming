a2ab9c84fd2b5ff69fd23cbe50007953a416fc07
Adam <adamdiallo2k@gmail.com>
2022-11-14 02:37:33 -0800

    edit h file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_h
+#define MAIN_h
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,3 +16,4 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+#endif
