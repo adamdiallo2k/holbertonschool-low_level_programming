386c761099b4e6c911d94f6c7f9baed176025684
Adam <adamdiallo2k@gmail.com>
2022-11-15 07:22:09 -0800

    fix the betty trailing space

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -9,7 +9,8 @@
  * Return: nothing
  */
 
-void before_main(void) 
+void before_main(void)
 {
-   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
