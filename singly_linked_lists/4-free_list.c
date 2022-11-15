2ada8850e590f71176c4f12a4c2f9e252e211ca8
Adam <adamdiallo2k@gmail.com>
2022-11-15 05:59:42 -0800

    free a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Entry point
+ * Description: 'Write a function that frees a list_t list.'
+ * Return: nothing
+ * @head: list_t  parameter
+ */
+
+void free_list(list_t *head)
+{
+	while (!head)
+	{
+	free(head->str);	
+	head = head->next;
+	}
+	free(head);
+}
+
