d4a97b7d76e80c4d9816dfecea5cb5df4d7bc0e7
Adam <adamdiallo2k@gmail.com>
2022-11-15 07:20:26 -0800

    advanced task about print before main

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * before_main - Entry point
+ * Description: 'Write a function that frees a list_t list.'
+ * Return: nothing
+ */
+
+void before_main(void) 
+{
+   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
