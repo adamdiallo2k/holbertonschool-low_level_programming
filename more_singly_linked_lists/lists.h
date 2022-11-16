aff9fb0f635cbb9831419d0c3c243473abaeea37
Adam <adamdiallo2k@gmail.com>
2022-11-16 00:43:09 -0800

    add the protection of double inclusion

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LIST_H
+#define LIST_H
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -12,3 +14,4 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+#endif
