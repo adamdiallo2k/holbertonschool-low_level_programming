dfa815365f5ddb7c57763bbcc09d4a08fff9982e
Adam <adamdiallo2k@gmail.com>
2022-11-16 01:55:46 -0800

    frees a listint_t list.

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ * Description: 'Write a function that frees a list_t list.'
+ * Return: nothing
+ * @head: listint_t  parameter
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+	while (head)
+	{
+	head = head->next;
+	}
+	free(head);
+	}
+}
