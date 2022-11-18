338715975cb8997112c5d18fb03c0eb97e550eaa
Adam <adamdiallo2k@gmail.com>
2022-11-18 13:44:10 -0800

    task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,51 +1,15 @@
-#include <string.h>
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
-/**
-* power - Entry point
-* Description: 'converts a binary number to an unsigned int..'
-* Return: int
-* @base: const charb
-* @a: unsigned int a
-*/
-
-unsigned long int power(unsigned long int base, unsigned long int a)
-{
-	if (a != 0)
-		return (base * power(base, a - 1));
-	else
-		return (1);
-}
+#include <stdio.h>
 
 /**
-* binary_to_uint - Entry point
-* Description: 'converts a binary number to an unsigned int..'
-* Return: int
-* @n: const charb
-*/
-
+ * print_binary - converts unsigned int to binary
+ * @n: unsigned long int given
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long result = 0;
-	unsigned long int i = 0;
-	if (n == 0)
-	{
-	
-	}
-	while (n != 0)
-	{
-	if (power(2,i) > n)
-        {
-		result += power(10,i - 1);
-		n = n - power(2,i - 1);
-		i = 0;
-	}
-	i++;
-	}
-	if (result == 0)
-		_putchar(0 + '0');
-	else
-		printf("%li",result);
+	if (n > 1)
+		print_binary(n >> 1);
 
+	_putchar((n & 1) + '0');
 }
