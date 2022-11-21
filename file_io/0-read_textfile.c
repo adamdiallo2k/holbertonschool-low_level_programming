dda67441e74538dbc662eec05f5d4bedb141bb94
Adam <adamdiallo2k@gmail.com>
2022-11-21 12:58:00 -0800

    edit 0 task

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,20 +6,31 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+* read_textfile - Entry point
+* Description: 'read a text file and print it'
+* Return: ssize_t
+* @filename: char  parameter
+* @letters: size_t parameter
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 
 	char *buf = malloc(sizeof(char) * letters);
-	fd = open(filename,O_RDONLY);
 	
+	if (buf == NULL || filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
 		return (0);
 	}
 	read(fd, buf, letters);
-	
+
 	close(fd);
-	
-	return (write(STDOUT_FILENO,buf,strlen(buf)));
+
+	return (write(STDOUT_FILENO, buf, strlen(buf)));
 }
