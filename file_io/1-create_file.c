518916d3dcec12062d788c4aced1729a30035b71
Adam <adamdiallo2k@gmail.com>
2022-11-22 00:57:01 -0800

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+* create_file - Entry point
+* Description: 'read a text file and print it'
+* Return: ssize_t
+* @filename: char parameter
+* @text_content: char parameterr
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		printf("file name null");
+		return (-1);
+	}
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		printf("file path");
+		return (-1);
+	}
+	write(fd, text_contnt, strlen(text_content));
+
+	close(fd);
+
+	return (1);
+}
