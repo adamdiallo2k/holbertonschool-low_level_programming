06c15a3e5902e3b3a46b194351807c60be805c22
Adam <adamdiallo2k@gmail.com>
2022-11-22 01:11:26 -0800

    task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+* create_file - Entry point
+* Description: 'read a text file and print it'
+* Return: ssize_t
+* @filename: char parameter
+* @text_content: char parameterr
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+
+	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0600);
+
+	if (fd == -1 || filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+
+	close(fd);
+
+	return (1);
+}
+
