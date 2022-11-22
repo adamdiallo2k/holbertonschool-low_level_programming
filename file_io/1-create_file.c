fe1c8c3248392bddb8d124b56784c40050fdb998
Adam <adamdiallo2k@gmail.com>
2022-11-22 01:02:26 -0800

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,11 +21,12 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (fd == -1 || filename == NULL || text_content == NULL)
+	if (fd == -1 || filename == NULL)
 	{
 		return (-1);
 	}
-	write(fd, text_content, strlen(text_content));
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
 
 	close(fd);
 
