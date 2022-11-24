d9ab5c4a2fdced5a794a9c65478337212645c011
Adam <adamdiallo2k@gmail.com>
2022-11-24 10:46:15 -0800

    task 1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+* hash_djb2 - Entry point
+* Description: 'converts a binary number to an unsigned int..'
+* Return: hash
+* @str: const unsigned char str
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
