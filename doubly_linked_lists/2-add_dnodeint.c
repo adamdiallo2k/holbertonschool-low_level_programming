b18503119981a7a443f52793e7de6413e828039d
Adam <adamdiallo2k@gmail.com>
2022-11-24 12:03:30 -0800

    task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+* add_dnodeint - Entry point
+* Description: 'returns the sum of all its parameters.'
+* Return: int
+* @head: list_t  parameter
+* @n: char parameter
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (!head)
+		return (0);
+	new->n = n;
+	new->next = (*head);
+
+	*head = new;
+
+	return (new);
+}
