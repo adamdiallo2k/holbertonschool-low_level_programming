49c2647d87c87cf96065ef34ee66b104028f9f81
Adam <adamdiallo2k@gmail.com>
2022-11-25 13:37:30 -0800

    task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - Entry point
+* Description: 'returns the number of nth node'
+* Return: size_t
+* @head: list_t  parameter
+* @index: int parameter
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node = head;
+	unsigned int count = 0;
+
+	while (node)
+	{
+		if (count == index)
+			return (node);
+		count++;
+		node = node->next;
+	}
+
+	return (NULL);
+}
