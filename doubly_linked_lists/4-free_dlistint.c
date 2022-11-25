1ea2dc680052718719a5805958458ea96807e023
Adam <adamdiallo2k@gmail.com>
2022-11-25 13:31:01 -0800

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ * Description: 'Write a function that frees a list_t list.'
+ * Return: nothing
+ * @head: listint_t  parameter
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	if (head)
+	{
+	while (head)
+	{
+	node = head;
+	head = head->next;
+	free(node);
+	}
+	}
+}
